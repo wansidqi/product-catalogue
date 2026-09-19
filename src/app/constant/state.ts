b8ad35306aa28e27d1a192bc4149c8218b879d2b
wansidqi <98792161+wansidqi@users.noreply.github.com>
2026-09-19 09:46:51 +0800

    handle state for loading, error and retry

---
+++ src/app/constant/state.ts
@@ -0,0 +1,6 @@
+export enum State {
+    LOADING = "loading",
+    SUCCESS = "success",
+    ERROR = "error",
+    RETRY = "retry",
+}
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,13 +4,31 @@ import { Color } from "./constant/color";
 import { useGetProducts } from "./services/product-service";
 
 export default function Index() {
-  const { data, fetchNextPage, refetch } = useGetProducts();
+  const { data, fetchNextPage, refetch, isLoading, isError } = useGetProducts();
 
   return (
     <View className={`flex-1 ${Color.PAGE} text-slate-200`}>
       <Text className="text-2xl font-bold text-blue-500 text-center">
         Product Catalog
       </Text>
+
+      {isLoading && (
+        <View
+          className={`flex-1 ${Color.PAGE} text-slate-200 flex-1 h-screen justify-center items-center`}
+        >
+          <Text className="text-slate-200 text-center">Loading...</Text>
+        </View>
+      )}
+
+      {isError && (
+        <View
+          className={`flex-1 ${Color.PAGE} text-slate-200 flex-1 h-screen justify-center items-center`}
+        >
+          <Text className="text-slate-200 text-center">Error load data</Text>
+          <Button title="Retry" onPress={() => refetch()} />
+        </View>
+      )}
+
       <FlatList
         numColumns={2}
         columnWrapperStyle={{ justifyContent: "space-evenly" }}
